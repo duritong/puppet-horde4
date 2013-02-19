6b5287efd0a304d71990758bf3f95446eb16339a
o <o@immerda.ch>
2013-02-19 19:46:58 +0100

    new horde/passwd needs php-ldap extension

--- manifests/base.pp
+++ manifests/base.pp
@@ -4,6 +4,7 @@ class horde4::base {
   include php::extensions::crypt_blowfish
   include php::extensions::pgsql
   include php::extensions::xml
+  include php::extensions::ldap
   include php::packages::geoip
   include php::packages::lzf
   include php::packages::services_weather
